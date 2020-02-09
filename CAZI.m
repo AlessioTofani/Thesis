d3ada4b9e60cfa9e46bc025c528b4f2986e1bdd7
alessiotofani <tofani94@gmail.com>
2020-02-09 19:00:40 +0100

    CAZI algorithm added

---
+++ CAZI.m
@@ -0,0 +1,156 @@
+function CAZI(initial_thetas, H, sigma, gamma, x, max_order, N, e, theta_1, theta_2, m)
+%Function for guaranteed system identification
+%Based on the paper "Bounded Error Identification of Systems With Time-Varying Parameters"
+
+%Parameters
+%initial_thetas - initial values
+%H - initial matrix of the search zonotope
+%sigma - value of the error
+%gamma - vector of the expansion factors
+%x - vector of the system output
+%regressor - regression vector
+%max_order - maximum order of the computed zonotopes
+%N - number of iterations
+%e - error
+%theta_1
+%theta_2
+%m - number of measurements
+
+[dimension1, dimension2] = size(initial_thetas); %get the number of parameters to be estimated
+parameters_number = dimension1; 
+
+p = initial_thetas; %initialization of the zonotope's center 
+[nrows,ncolumns] = size(H); 
+order = ncolumns; %extraction of the order of the zonotope
+Tbest = cell(1,N+2); %instantiation of the matrix containing the T matrixes
+vbest = cell(1,N+2); %instantiation of the matrix containing the v vectors
+Gamma = diag(gamma); %diagonal matrix of the expansion factors
+
+k = 3;
+while k <= N + 2 %iteration over the number of iterations
+    i = 1;
+    matrix = horzcat(p,H);
+    Z = zonotope(matrix);
+    Zhalf = halfspace(Z); %creation of the half space representation
+    half = Zhalf.halfspace; %extracing the values
+    A = half.H;
+    b = half.K;
+    
+    while i <= m %iteration over the number of measurements
+        b1 = x(k) - e(k);
+        b2 = x(k) - e(k);
+        phi_u = ones(1,2);
+        phi_u(1) = x(k-1) + abs(sigma * x(k-1));
+        phi_u(2) = -x(k-2) + abs(sigma * x(k-2));
+        phi_l = ones(1,2);
+        phi_l(1) = x(k-1) - abs(sigma * x(k-1));
+        phi_l(2) = -x(k-2) - abs(sigma * x(k-2));
+        
+        %first strip
+        Abase = vertcat(A,phi_l);
+        bbase = vertcat(b,b2);
+        Aeq = []; %equality constraints
+        beq = []; %equality constraints
+        x0 = [1,1]; %initial guess
+        lb = [0,0]; %lower bounds
+        up = []; %upper bounds
+        options = optimoptions('fmincon','Display','off'); 
+
+        theta1 = fmincon(@objective, x0, Abase, bbase, Aeq, beq, lb, up, @nlcon, options, phi_u, phi_u, b1);
+        delta1_star = objective(theta1,phi_u,phi_u,b1);
+        %strip variables
+        c1 = phi_u.';
+        d1 = b1 + delta1_star/2 * norm(phi_u);
+        sigma1 = delta1_star/2 * norm(phi_u);
+        
+        %intersection of the zonotope and the strip
+        [T_set,v_set,volumes_list] = intersection(order,p,d1,c1,H,sigma1);
+        [min_volume, jstar] = min(volumes_list); %get the smallest volume and its corresponding index (j*)
+        H_new = T_set{jstar};
+        v_new = v_set{jstar};
+        zono_matrix = horzcat(v_new,H_new);
+        Z_new = zonotope(zono_matrix);
+        Zhalf_new = halfspace(Z_new); %creation of the half space representation
+        half_new = Zhalf_new.halfspace; %extracing the values
+        A = half_new.H;
+        b = half_new.K;
+        Abase = vertcat(A,phi_l);
+        bbase = vertcat(b,b2);
+
+        [nrows,ncolumns] = size(H_new);
+        new_order = ncolumns;
+        
+        %second strip
+        theta2 = fmincon(@objective, x0, Abase, bbase, Aeq, beq, lb, up, @nlcon, options, phi_l, phi_u, b2);
+        delta2_star = objective(theta2,phi_l,phi_u,b2);
+
+        %strip variables
+        c2 = phi_l.';
+        d2 = b2 + delta2_star/2 * norm(phi_l);
+        sigma2 = delta2_star/2 * norm(phi_l);
+        
+        %intersection of the zonotope and the strip
+        [T_set_new,v_set_new,volumes_list_new] = intersection(new_order,v_new,d2,c2,H_new,sigma2);
+        [min_volume, jstar_new] = min(volumes_list_new); %get the smallest volume and its corresponding index (j*)
+        H_fin = T_set_new{jstar_new};
+        v_fin = v_set_new{jstar_new};
+        %zono_matrix = horzcat(v_fin,H_fin);
+        %Z_fin = zonotope(zono_matrix);
+        
+        i = i +1;
+    end
+    H_fin = horzcat(H_fin,Gamma); %set expansion
+
+    if order >= max_order %zonotope reduction 
+        zono_matrix = horzcat(v_fin,H_fin);
+        z = zonotope(zono_matrix);
+        z_reduced = reduce(z,'girard',max_order);
+        H_fin = generators(z_reduced);
+        v_fin = center(z_reduced);
+    end
+    H = H_fin;
+    p = v_fin;
+    Tbest{k} = H;
+    vbest{k} = p;
+    [nrows,ncolumns] = size(H);
+    order = ncolumns;
+    k = k + 1;
+end
+
+Tbest = Tbest(1,4:102); %cut the first empty values
+vbest = vbest(1,4:102); %cut the first empty values
+steps = N - 1; %number of steps for the graphs
+
+%calculations of the limits of the zonotopes at every instant k
+bounds = cell(1,steps);
+for i = 1:steps
+    bounds{i} = sum(abs(Tbest{i}),2);
+end
+
+%visualization of the parameters
+for i = 1:parameters_number
+    figure();
+    upper = zeros(1,steps);
+    lower = zeros(1,steps);
+    centers = zeros(1,steps);
+    for j = 1:steps
+        current_bound = bounds{j};
+        current_bound = current_bound(i,:);
+        actual_value = vbest{j};
+        center_temp = actual_value(i,:);
+        centers(j) = center_temp;
+        upper(j) = current_bound + center_temp;
+        lower(j) = center_temp - current_bound;
+    end
+    hold on;
+    title("θ_" + i);
+    plot(centers,'g');
+    plot(upper,'b');
+    plot(lower,'b');
+    if i == 1
+        plot(theta_1,'k');
+    else
+        plot(theta_2, 'k');
+    end
+    xlabel('k');
+end
