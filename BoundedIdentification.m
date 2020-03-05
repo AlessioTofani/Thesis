584a986e2c393a9966add994f3a772c807228c2b
alessiotofani <tofani94@gmail.com>
2020-03-05 16:38:58 +0100

    alternative visualization added

--- BoundedIdentification.m
+++ BoundedIdentification.m
@@ -22,6 +22,7 @@ Tbest = cell(1,N); %instantiation of the matrix containing the T matrixes
 vbest = cell(1,N); %instantiation of the matrix containing the v vectors
 Gamma = diag(gamma); %diagonal matrix of the expansion factors
 max_order = max_segments / parameters_number; %calculating the maximum order of the zonotopes
+time_var = 1; %specify if use also the alternative visualization
 
 for index = 1:N
     T_set = cell(1,order + 1); %list of the matrixes T
@@ -57,7 +58,7 @@ for i = 1:N
     bounds{i} = sum(abs(Tbest{i}),2);
 end
 
-%visualisation of the parameters
+%visualization of the parameters
 for i = 1:parameters_number
     figure();
     upper = zeros(1,N); %upper_bounds
@@ -80,5 +81,30 @@ for i = 1:parameters_number
     xlabel('k');
     ylabel("θ_" + i);
 end
-    
+
+%alternative way to visualize the paramters with their bounds as zonotopes
+if time_var == 1
+    iteration_count = 25; %specify every many iterations to display the zonotope
+    for i = 1:parameters_number
+        figure();
+        cc = lines; %color map for the tight strips
+        centers = zeros(1,N); %center
+        for j = 1:N
+            actual_value = vbest{j};
+            center_temp = actual_value(i,:);
+            centers(j) = center_temp;
+            if mod(j,iteration_count) == 0 || (j == 1) 
+                hold on;
+                v = zeros(parameters_number,1);
+                v(1) = j;
+                v(2) = center_temp(1);
+                zono_matrix = horzcat(v, Tbest{j});
+                z = zonotope(zono_matrix);
+                plot(z, [1 2],'color',cc(j+1,:), 'LineWidth',1.5);
+            end
+        end
+        plot(centers,'g', 'LineWidth',1.5);
+    end
+end
+
 end
