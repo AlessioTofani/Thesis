73493e35107db850b5e56f7814fdb827542dda90
alessiotofani <tofani94@gmail.com>
2020-04-22 19:14:17 +0200

    minor changes

--- BoundedIdentification.m
+++ BoundedIdentification.m
@@ -1,4 +1,4 @@
-function BoundedIdentification(theta_c, H, sigma, gamma, y, regressor, max_segments, N)
+function [vbest,Tbest] = BoundedIdentification(theta_c, H, sigma, gamma, y, regressor, max_segments, N)
 %Function for guaranteed system identification
 %Based on the paper "Bounded Error Identification of Systems With Time-Varying Parameters"
 
@@ -52,62 +52,4 @@ for index = 1:N
     [nrows,ncolumns] = size(H);
     order = ncolumns;
 end
-
-%calculations of the limits of the zonotopes at every instant k
-bounds = cell(1,N);
-for i = 1:N
-    bounds{i} = sum(abs(Tbest{i}),2);
-end
-
-%visualization of the parameters
-for i = 1:parameters_number
-    figure();
-    upper = zeros(1,N); %upper_bounds
-    lower = zeros(1,N); %lower_bounds
-    centers = zeros(1,N); %center
-    for j = 1:N
-        current_bound = bounds{j};
-        current_bound = current_bound(i,:);
-        actual_value = vbest{j};
-        center_temp = actual_value(i,:);
-        centers(j) = center_temp;
-        upper(j) = current_bound + center_temp;
-        lower(j) = center_temp - current_bound;
-    end
-    hold on;
-    set(gca,'FontSize',18);
-    plot(centers,'g', 'LineWidth',1.5);
-    plot(upper,'b','LineWidth',1.5);
-    plot(lower,'b','LineWidth',1.5);
-    xlabel('k');
-    ylabel("θ_" + i);
-end
-
-%alternative way to visualize the paramters with their bounds as zonotopes
-if time_var == 1
-    iteration_count = 20; %specify every many iterations to display the zonotope
-    for i = 1:parameters_number
-        figure();
-        cc = lines; %color map for the tight strips
-        centers = zeros(1,N); %center
-        for j = 1:N
-            actual_value = vbest{j};
-            center_temp = actual_value(i,:);
-            centers(j) = center_temp;
-            if mod(j,iteration_count) == 0 || (j == 1) 
-                hold on;
-                v = zeros(parameters_number,1);
-                v(1) = j;
-                v(2) = center_temp(1);
-                zono_matrix = horzcat(v, Tbest{j});
-                z = zonotope(zono_matrix);
-                plot(z, [1 2],'color',cc(j+1,:), 'LineWidth',1.5);
-            end
-        end
-        plot(centers,'g', 'LineWidth',1.5);
-        xlabel('k');
-        ylabel("θ_" + i);
-    end
-end
-
 end
